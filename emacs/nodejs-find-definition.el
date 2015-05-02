27c2327d96bfe9ef009333bb71f033f993721a7b
yamadapc <tacla.yamada@gmail.com>
2015-05-02 14:54:05 -0300

    Update `nodejs-find-definition.el`

--- emacs/nodejs-find-definition.el
+++ emacs/nodejs-find-definition.el
@@ -3,24 +3,25 @@
 
 (defun nodejs-find-definition (line col)
   (interactive (list (count-lines 1 (point)) (current-column)))
-  (let ((command (format "find-definition -l %d -c %d -n %s -r %s -f %s"
+  (let* ((command (format "find-definition -l %d -c %d -n %s -r %s -f %s"
                          line col
                          (concat (projectile-project-root) "/node_modules")
                          (concat (projectile-project-root) "/core")
-                         (buffer-file-name))))
-  (message command)
-  (let ((output (shell-command-to-string command)))
-    (message output)
-    (let* ((result (json-read-from-string output))
-           (def-filepath (cdr (assoc 'path result)))
+                         (buffer-file-name)))
+         (output (shell-command-to-string command))
+         (result (json-read-from-string output)))
+
+    (when (cdr (assoc 'error result))
+      (error (cdr (assoc 'error result))))
+
+    (let* ((def-filepath (cdr (assoc 'path result)))
            (def-loc (cdr (assoc 'start (cdr (assoc 'loc result)))))
            (def-col (cdr (assoc 'column def-loc)))
            (def-line (cdr (assoc 'line def-loc))))
-      (message (pp def-loc))
       (find-file def-filepath)
       (let ((current-prefix-arg def-line))
         (call-interactively 'goto-line))
-      (move-to-column def-col)))))
+      (move-to-column def-col))))
 
 (require 'evil-leader)
 (evil-leader/set-key;; -for-mode 'js2-mode
